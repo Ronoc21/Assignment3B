14b7e20c326a365276be69a801bd491ab44ce9b1
Conor <conor.okeefe.2121@gmail.com>
2021-04-16 08:38:54 -0700

    min_max.py

--- factors.py
+++ factors.py
@@ -1,10 +1,10 @@
-#Conor Smith
+# Conor Smith
 # Date: 04/14/2021
 # Description: Asks user for numbers, factors that number
 
-#Asks user to input the number they want factores
-num = int(input("Please enter a positive integer:"))
-print("The factors of", num, "are")
+#Asks user to input the number they want factoresd
+num = int(input("Please enter a positive integer: "))
+print("The factors of", num, "are:")
 #uses for loop to mod operator user's number by i range
 for i in range (1, num +1):
     if num % i == 0:
